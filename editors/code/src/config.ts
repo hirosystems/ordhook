0b43df8845ef3b729296f27d57260bc0c14b9133
Charlie <2747302+CharlieC3@users.noreply.github.com>
2021-06-30 20:26:53 -0400

    ci: release workflow (#30)
    * ci: update release workflow
    
    * ci: formatting and linting
    
    * ci: adjust extension release settings

--- editors/code/src/config.ts
+++ editors/code/src/config.ts
@@ -6,84 +6,84 @@ export type UpdatesChannel = "stable" | "nightly";
 export const NIGHTLY_TAG = "nightly";
 
 export class Config {
-    readonly extensionId = "hirosystems.clarity-lsp";
+	readonly extensionId = "hirosystems.clarity-lsp";
 
-    private readonly rootSection = "clarity-lsp";
-    private readonly requiresReloadOpts = [
-        "serverPath",
-        "cargo",
-        "files",
-        "highlighting",
-        "updates.channel",
-    ]
-        .map(opt => `${this.rootSection}.${opt}`);
+	private readonly rootSection = "clarity-lsp";
+	private readonly requiresReloadOpts = [
+		"serverPath",
+		"cargo",
+		"files",
+		"highlighting",
+		"updates.channel",
+	]
+		.map(opt => `${this.rootSection}.${opt}`);
 
-    readonly package: {
-        version: string;
-        releaseTag: string | null;
-        enableProposedApi: boolean | undefined;
-    } = vscode.extensions.getExtension(this.extensionId)!.packageJSON;
+	readonly package: {
+		version: string;
+		releaseTag: string | null;
+		enableProposedApi: boolean | undefined;
+	} = vscode.extensions.getExtension(this.extensionId)!.packageJSON;
 
-    readonly globalStoragePath: string;
+	readonly globalStoragePath: string;
 
-    constructor(ctx: vscode.ExtensionContext) {
-        this.globalStoragePath = ctx.globalStoragePath;
-        vscode.workspace.onDidChangeConfiguration(this.onDidChangeConfiguration, this, ctx.subscriptions);
-        this.refreshLogging();
-    }
+	constructor(ctx: vscode.ExtensionContext) {
+		this.globalStoragePath = ctx.globalStoragePath;
+		vscode.workspace.onDidChangeConfiguration(this.onDidChangeConfiguration, this, ctx.subscriptions);
+		this.refreshLogging();
+	}
 
-    private refreshLogging() {
-        log.setEnabled(this.traceExtension);
-        log.debug(
-            "Extension version:", this.package.version,
-            "using configuration:", this.cfg
-        );
-    }
+	private refreshLogging() {
+		log.setEnabled(this.traceExtension);
+		log.debug(
+			"Extension version:", this.package.version,
+			"using configuration:", this.cfg
+		);
+	}
 
-    private async onDidChangeConfiguration(event: vscode.ConfigurationChangeEvent) {
-        this.refreshLogging();
+	private async onDidChangeConfiguration(event: vscode.ConfigurationChangeEvent) {
+		this.refreshLogging();
 
-        const requiresReloadOpt = this.requiresReloadOpts.find(
-            opt => event.affectsConfiguration(opt)
-        );
+		const requiresReloadOpt = this.requiresReloadOpts.find(
+			opt => event.affectsConfiguration(opt)
+		);
 
-        if (!requiresReloadOpt) return;
+		if (!requiresReloadOpt) return;
 
-        const userResponse = await vscode.window.showInformationMessage(
-            `Changing "${requiresReloadOpt}" requires a reload`,
-            "Reload now"
-        );
+		const userResponse = await vscode.window.showInformationMessage(
+			`Changing "${requiresReloadOpt}" requires a reload`,
+			"Reload now"
+		);
 
-        if (userResponse === "Reload now") {
-            await vscode.commands.executeCommand("workbench.action.reloadWindow");
-        }
-    }
+		if (userResponse === "Reload now") {
+			await vscode.commands.executeCommand("workbench.action.reloadWindow");
+		}
+	}
 
-    // We don't do runtime config validation here for simplicity. More on stackoverflow:
-    // https://stackoverflow.com/questions/60135780/what-is-the-best-way-to-type-check-the-configuration-for-vscode-extension
+	// We don't do runtime config validation here for simplicity. More on stackoverflow:
+	// https://stackoverflow.com/questions/60135780/what-is-the-best-way-to-type-check-the-configuration-for-vscode-extension
 
-    private get cfg(): vscode.WorkspaceConfiguration {
-        return vscode.workspace.getConfiguration(this.rootSection);
-    }
+	private get cfg(): vscode.WorkspaceConfiguration {
+		return vscode.workspace.getConfiguration(this.rootSection);
+	}
 
-    get serverPath() { return this.cfg.get<null | string>("serverPath")!; }
-    get channel() { return this.cfg.get<UpdatesChannel>("updates.channel")!; }
-    get askBeforeDownload() { return this.cfg.get<boolean>("updates.askBeforeDownload")!; }
-    get highlightingSemanticTokens() { return this.cfg.get<boolean>("highlighting.semanticTokens")!; }
-    get traceExtension() { return this.cfg.get<boolean>("trace.extension")!; }
+	get serverPath() { return this.cfg.get<null | string>("serverPath")!; }
+	get channel() { return this.cfg.get<UpdatesChannel>("updates.channel")!; }
+	get askBeforeDownload() { return this.cfg.get<boolean>("updates.askBeforeDownload")!; }
+	get highlightingSemanticTokens() { return this.cfg.get<boolean>("highlighting.semanticTokens")!; }
+	get traceExtension() { return this.cfg.get<boolean>("trace.extension")!; }
 
-    get inlayHints() {
-        return {
-            typeHints: this.cfg.get<boolean>("inlayHints.typeHints")!,
-            parameterHints: this.cfg.get<boolean>("inlayHints.parameterHints")!,
-            chainingHints: this.cfg.get<boolean>("inlayHints.chainingHints")!,
-            maxLength: this.cfg.get<null | number>("inlayHints.maxLength")!,
-        };
-    }
+	get inlayHints() {
+		return {
+			typeHints: this.cfg.get<boolean>("inlayHints.typeHints")!,
+			parameterHints: this.cfg.get<boolean>("inlayHints.parameterHints")!,
+			chainingHints: this.cfg.get<boolean>("inlayHints.chainingHints")!,
+			maxLength: this.cfg.get<null | number>("inlayHints.maxLength")!,
+		};
+	}
 
-    get checkOnSave() {
-        return {
-            command: this.cfg.get<string>("checkOnSave.command")!,
-        };
-    }
+	get checkOnSave() {
+		return {
+			command: this.cfg.get<string>("checkOnSave.command")!,
+		};
+	}
 }
--- editors/code/src/main.ts
+++ editors/code/src/main.ts
@@ -12,24 +12,24 @@ import * as os from "os";
 import {
 	LanguageClient,
 	LanguageClientOptions,
-    Executable,
+	Executable,
 } from 'vscode-languageclient';
 
 let client: LanguageClient;
 
 export async function activate(context: ExtensionContext) {
-	
+
 	const config = new Config(context);
-    const state = new PersistentState(context.globalState);
+	const state = new PersistentState(context.globalState);
 
 	const serverPath = await bootstrap(config, state);
 
-	let serverOptions: Executable = {
+	const serverOptions: Executable = {
 		command: serverPath
 	};
 
 	// Options to control the language client
-	let clientOptions: LanguageClientOptions = {
+	const clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
 		documentSelector: [{ scheme: 'file', language: 'clarity' }],
 		synchronize: {
@@ -45,7 +45,7 @@ export async function activate(context: ExtensionContext) {
 		serverOptions,
 		clientOptions
 	);
-	
+
 	// Start the client. This will also launch the server
 	client.start();
 }
@@ -58,133 +58,133 @@ export async function deactivate() {
 }
 
 async function bootstrap(config: Config, state: PersistentState): Promise<string> {
-    await fs.mkdir(config.globalStoragePath, { recursive: true });
+	await fs.mkdir(config.globalStoragePath, { recursive: true });
 
-    await bootstrapExtension(config, state);
-    const path = await bootstrapServer(config, state);
+	await bootstrapExtension(config, state);
+	const path = await bootstrapServer(config, state);
 
-    return path;
+	return path;
 }
 
 async function bootstrapExtension(config: Config, state: PersistentState): Promise<void> {
-    if (config.package.releaseTag === null) return;
-    if (config.channel === "stable") {
-        if (config.package.releaseTag === NIGHTLY_TAG) {
-            void vscode.window.showWarningMessage(
-                `You are running a nightly version of clarity-lsp extension. ` +
-                `To switch to stable, uninstall the extension and re-install it from the marketplace`
-            );
-        }
-        return;
-    };
+	if (config.package.releaseTag === null) return;
+	if (config.channel === "stable") {
+		if (config.package.releaseTag === NIGHTLY_TAG) {
+			void vscode.window.showWarningMessage(
+				`You are running a nightly version of clarity-lsp extension. ` +
+				`To switch to stable, uninstall the extension and re-install it from the marketplace`
+			);
+		}
+		return;
+	};
 
-    const lastCheck = state.lastCheck;
-    const now = Date.now();
+	const lastCheck = state.lastCheck;
+	const now = Date.now();
 
-    const anHour = 60 * 60 * 1000;
-    const shouldDownloadNightly = state.releaseId === undefined || (now - (lastCheck ?? 0)) > anHour;
+	const anHour = 60 * 60 * 1000;
+	const shouldDownloadNightly = state.releaseId === undefined || (now - (lastCheck ?? 0)) > anHour;
 
-    if (!shouldDownloadNightly) return;
+	if (!shouldDownloadNightly) return;
 
-    const release = await fetchRelease("nightly").catch((e) => {
-        log.error(e);
-        if (state.releaseId === undefined) { // Show error only for the initial download
-            vscode.window.showErrorMessage(`Failed to download clarity-lsp nightly ${e}`);
-        }
-        return undefined;
-    });
-    if (release === undefined || release.id === state.releaseId) return;
+	const release = await fetchRelease("nightly").catch((e) => {
+		log.error(e);
+		if (state.releaseId === undefined) { // Show error only for the initial download
+			vscode.window.showErrorMessage(`Failed to download clarity-lsp nightly ${e}`);
+		}
+		return undefined;
+	});
+	if (release === undefined || release.id === state.releaseId) return;
 
-    const userResponse = await vscode.window.showInformationMessage(
-        "New version of clarity-lsp (nightly) is available (requires reload).",
-        "Update"
-    );
-    if (userResponse !== "Update") return;
+	const userResponse = await vscode.window.showInformationMessage(
+		"New version of clarity-lsp (nightly) is available (requires reload).",
+		"Update"
+	);
+	if (userResponse !== "Update") return;
 
-    const artifact = release.assets.find(artifact => artifact.name === "clarity-lsp.vsix");
-    assert(!!artifact, `Bad release: ${JSON.stringify(release)}`);
+	const artifact = release.assets.find(artifact => artifact.name === "clarity-lsp.vsix");
+	assert(!!artifact, `Bad release: ${JSON.stringify(release)}`);
 
-    const dest = path.join(config.globalStoragePath, "clarity-lsp.vsix");
-    await download(artifact.browser_download_url, dest, "Downloading clarity-lsp extension");
+	const dest = path.join(config.globalStoragePath, "clarity-lsp.vsix");
+	await download(artifact.browser_download_url, dest, "Downloading clarity-lsp extension");
 
-    await vscode.commands.executeCommand("workbench.extensions.installExtension", vscode.Uri.file(dest));
-    await fs.unlink(dest);
+	await vscode.commands.executeCommand("workbench.extensions.installExtension", vscode.Uri.file(dest));
+	await fs.unlink(dest);
 
-    await state.updateReleaseId(release.id);
-    await state.updateLastCheck(now);
-    await vscode.commands.executeCommand("workbench.action.reloadWindow");
+	await state.updateReleaseId(release.id);
+	await state.updateLastCheck(now);
+	await vscode.commands.executeCommand("workbench.action.reloadWindow");
 }
 
 async function bootstrapServer(config: Config, state: PersistentState): Promise<string> {
-    const path = await getServer(config, state);
-    if (!path) {
-        throw new Error(
-            "Clarity Language Server is not available. " +
-            "Please, ensure its [proper installation](https://github.com/hirosystems/clarity-lsp)."
-        );
-    }
+	const path = await getServer(config, state);
+	if (!path) {
+		throw new Error(
+			"Clarity Language Server is not available. " +
+			"Please, ensure its [proper installation](https://github.com/hirosystems/clarity-lsp)."
+		);
+	}
 
-    // log.debug("Using server binary at", path);
+	// log.debug("Using server binary at", path);
 
-    // const res = spawnSync(path, ["--version"], { encoding: 'utf8' });
-    // log.debug("Checked binary availability via --version", res);
-    // log.debug(res, "--version output:", res.output);
-    // if (res.status !== 0) {
-    //     throw new Error(`Failed to execute ${path} --version`);
-    // }
+	// const res = spawnSync(path, ["--version"], { encoding: 'utf8' });
+	// log.debug("Checked binary availability via --version", res);
+	// log.debug(res, "--version output:", res.output);
+	// if (res.status !== 0) {
+	//     throw new Error(`Failed to execute ${path} --version`);
+	// }
 
-    return path;
+	return path;
 }
 
 async function getServer(config: Config, state: PersistentState): Promise<string | undefined> {
-    const explicitPath = process.env.__RA_LSP_SERVER_DEBUG ?? config.serverPath;
-    if (explicitPath) {
-        if (explicitPath.startsWith("~/")) {
-            return os.homedir() + explicitPath.slice("~".length);
-        }
-        return explicitPath;
-    };
-    if (config.package.releaseTag === null) return "clarity-lsp";
+	const explicitPath = process.env.__RA_LSP_SERVER_DEBUG ?? config.serverPath;
+	if (explicitPath) {
+		if (explicitPath.startsWith("~/")) {
+			return os.homedir() + explicitPath.slice("~".length);
+		}
+		return explicitPath;
+	};
+	if (config.package.releaseTag === null) return "clarity-lsp";
 
-    let binaryName: string | undefined = undefined;
-    if (process.arch === "x64" || process.arch === "ia32") {
-        if (process.platform === "linux") binaryName = "clarity-lsp-linux";
-        if (process.platform === "darwin") binaryName = "clarity-lsp-mac";
-        if (process.platform === "win32") binaryName = "clarity-lsp-windows.exe";
-    }
-    if (binaryName === undefined) {
-        vscode.window.showErrorMessage(
-            "Unfortunately we don't ship binaries for your platform yet. " +
-            "You need to manually clone clarity-lsp repository and " +
-            "run `cargo xtask install --server` to build the language server from sources. " +
-            "If you feel that your platform should be supported, please create an issue " +
-            "about that [here](https://github.com/hirosystems/clarity-lsp/issues) and we " +
-            "will consider it."
-        );
-        return undefined;
-    }
+	let binaryName: string | undefined = undefined;
+	if (process.arch === "x64" || process.arch === "ia32") {
+		if (process.platform === "linux") binaryName = "clarity-lsp-linux";
+		if (process.platform === "darwin") binaryName = "clarity-lsp-mac";
+		if (process.platform === "win32") binaryName = "clarity-lsp-windows.exe";
+	}
+	if (binaryName === undefined) {
+		vscode.window.showErrorMessage(
+			"Unfortunately we don't ship binaries for your platform yet. " +
+			"You need to manually clone clarity-lsp repository and " +
+			"run `cargo xtask install --server` to build the language server from sources. " +
+			"If you feel that your platform should be supported, please create an issue " +
+			"about that [here](https://github.com/hirosystems/clarity-lsp/issues) and we " +
+			"will consider it."
+		);
+		return undefined;
+	}
 
-    const dest = path.join(config.globalStoragePath, binaryName);
-    const exists = await fs.stat(dest).then(() => true, () => false);
-    if (!exists) {
-        await state.updateServerVersion(undefined);
-    }
+	const dest = path.join(config.globalStoragePath, binaryName);
+	const exists = await fs.stat(dest).then(() => true, () => false);
+	if (!exists) {
+		await state.updateServerVersion(undefined);
+	}
 
-    if (state.serverVersion === config.package.version) return dest;
+	if (state.serverVersion === config.package.version) return dest;
 
-    if (config.askBeforeDownload) {
-        const userResponse = await vscode.window.showInformationMessage(
-            `Language server version ${config.package.version} for clarity-lsp is not installed.`,
-            "Download now"
-        );
-        if (userResponse !== "Download now") return dest;
-    }
+	if (config.askBeforeDownload) {
+		const userResponse = await vscode.window.showInformationMessage(
+			`Language server version ${config.package.version} for clarity-lsp is not installed.`,
+			"Download now"
+		);
+		if (userResponse !== "Download now") return dest;
+	}
 
-    const release = await fetchRelease(config.package.releaseTag);
-    const artifact = release.assets.find(artifact => artifact.name === binaryName);
-    assert(!!artifact, `Bad release: ${JSON.stringify(release)}`);
+	const release = await fetchRelease(config.package.releaseTag);
+	const artifact = release.assets.find(artifact => artifact.name === binaryName);
+	assert(!!artifact, `Bad release: ${JSON.stringify(release)}`);
 
-    await download(artifact.browser_download_url, dest, "Downloading clarity-lsp server", { mode: 0o755 });
-    await state.updateServerVersion(config.package.version);
-    return dest;
+	await download(artifact.browser_download_url, dest, "Downloading clarity-lsp server", { mode: 0o755 });
+	await state.updateServerVersion(config.package.version);
+	return dest;
 }
