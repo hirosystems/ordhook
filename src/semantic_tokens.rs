1df0bdddce989831729ac5794e60720a62d71014
Paul Saunders <coruscateor@users.noreply.github.com>
2021-08-11 19:32:08 +1200

    Worked on semantic tokens - WIP

--- src/semantic_tokens.rs
+++ src/semantic_tokens.rs
@@ -5,12 +5,18 @@ use lsp_types::{
     SemanticTokenModifier,
     SemanticTokenType,
     SemanticTokens,
-    SemanticTokensEdit
+    SemanticTokensEdit,
+    SemanticTokensParams,
+    SemanticTokensRangeResult
 };
 
 use String;
 
 use std::vec;
+
+use std::fs::read_to_string;
+
+use std::io::ErrorKind;
 
 /*
 use chrono::{
@@ -27,8 +33,14 @@ pub fn get_supported_token_types() -> Vec<SemanticTokenType>
 {
 
     let mut token_types = vec!();
+
+    token_types.push(SemanticTokenType::TYPE);
+
+    token_types.push(SemanticTokenType::FUNCTION);
+
+    token_types.push(SemanticTokenType::KEYWORD);
     
-    token_types.push(SemanticTokenType::new("block-height"));
+    /*token_types.push(SemanticTokenType::new("block-height"));
 
     token_types.push(SemanticTokenType::new("burn-block-height"));
 
@@ -44,9 +56,166 @@ pub fn get_supported_token_types() -> Vec<SemanticTokenType>
 
     token_types.push(SemanticTokenType::new("true"));
 
-    token_types.push(SemanticTokenType::new("tx-sender"));
+    token_types.push(SemanticTokenType::new("tx-sender"));*/
 
     token_types
+
+}
+
+pub fn get_supported_token_modifiers() -> Vec<SemanticTokenModifier>
+{
+
+    vec![]
+
+}
+
+pub fn try_get_SemanticTokenType(input: &str) -> Option<SemanticTokenType>
+{
+
+    match input
+    {
+
+        //Types
+
+        "tuple" |
+        "list" |
+        "response" |
+        "optional" |
+        "buff" |
+        "string-ascii" |
+        "string-utf8" |
+        "principal" |
+        "bool" |
+        "int" |
+        "uint" => {
+
+            return Some(SemanticTokenType::TYPE);
+
+        },
+
+        //Functions
+
+        "-" | // (subtract)
+        "*" | //(multiply)
+        "/" |//(divide)
+        "+" | //(add)
+        "<" | //(less than)
+        "<=" | //(less than or equal)
+        ">" | //(greater than)
+        ">=" | //(greater than or equal)
+        "and" |
+        "append" |
+        "as-contract" |
+        "as-max-len?" |
+        "asserts!" |
+        "at-block" |
+        "begin" |
+        "concat" |
+        "contract-call?" |
+        "contract-of" |
+        "default-to" |
+        "define-constant" |
+        "define-data-var" |
+        "define-fungible-token" |
+        "define-map" |
+        "define-non-fungible-token" |
+        "define-private" |
+        "define-public" |
+        "define-read-only" |
+        "define-trait" |
+        "element-at" |
+        "err" |
+        "filter" |
+        "fold" |
+        "ft-burn?" |
+        "ft-get-balance" |
+        "ft-get-supply" |
+        "ft-mint?" |
+        "ft-transfer?" |
+        "get" |
+        "get-block-info?" |
+        "hash160" |
+        "if" |
+        "impl-trait" |
+        "index-of" |
+        "is-eq" |
+        "is-err" |
+        "is-none" |
+        "is-ok" |
+        "is-some" |
+        "keccak256" |
+        "len" |
+        "let" |
+        "list" |
+        "log2" |
+        "map" |
+        "map-delete" |
+        "map-get?" |
+        "map-insert" |
+        "map-set" |
+        "match" |
+        "merge" |
+        "mod" |
+        "nft-burn?" |
+        "nft-get-owner?" |
+        "nft-mint?" |
+        "nft-transfer?" |
+        "not" |
+        "ok" |
+        "or" |
+        "pow" |
+        "principal-of?" |
+        "print" |
+        "secp256k1-recover?" |
+        "secp256k1-verify" |
+        "sha256" |
+        "sha512" |
+        "sha512/256" |
+        "some" |
+        "sqrti" |
+        "stx-burn?" |
+        "stx-get-balance" |
+        "stx-transfer?" |
+        "to-int" |
+        "to-uint" |
+        "try!" |
+        "tuple" |
+        "unwrap-err-panic" |
+        "unwrap-err!" |
+        "unwrap-panic" |
+        "unwrap!" |
+        "use-trait" |
+        "var-get" |
+        "var-set" |
+        "xor" => {
+
+            return Some(SemanticTokenType::FUNCTION);
+
+        },
+
+        //Keywords
+
+        "block-height" |
+        "burn-block-height" |
+        "contract-caller" |
+        "false" |
+        "is-in-regtest" |
+        "none" |
+        "stx-liquid-supply" |
+        "true" |
+        "tx-sender" =>
+        {
+
+            return Some(SemanticTokenType::KEYWORD);
+
+        },
+        _ => {
+
+            None
+
+        }
+
+    }
 
 }
 
@@ -123,7 +292,7 @@ impl SemanticTokensBuilder
 
     }
 
-    pub fn push_strs(&mut self, line: u32, char: u32, length: u32, token_type: &str, token_modifiers: Option<Vec<&str>>, supported_token_types: &Vec<SemanticTokenType>)
+    pub fn push_strs(&mut self, line: u32, char: u32, length: u32, token_type: &SemanticTokenType, token_modifiers: Option<Vec<&str>>, supported_token_types: &Vec<SemanticTokenType>)
     {
 
         let mut index: u32 = 0;
@@ -139,7 +308,7 @@ impl SemanticTokensBuilder
 
             index += 1;
 
-            if token_type == supported_token_type.as_str()
+            if token_type == supported_token_type
             {
 
                 found_index = index;
@@ -232,12 +401,12 @@ pub struct ParsedToken
 }
 */
 
-pub fn parse_text(text: &String)
+pub fn parse_text(text: &String) -> Vec::<String>
 {
 
     let mut parsedTokens = Vec::<String>::new();
 
-    let lines = Vec::<String>::new();
+    //let lines = Vec::<String>::new();
 
     //Tokenisation
 
@@ -382,6 +551,8 @@ pub fn parse_text(text: &String)
 
     }
 
+    parsedTokens
+
 }
 
 fn completeToken(parsedTokens: &mut Vec::<String>, currentToken:  &mut String)
@@ -390,5 +561,73 @@ fn completeToken(parsedTokens: &mut Vec::<String>, currentToken:  &mut String)
     parsedTokens.push(currentToken.clone());
 
     currentToken.clear();
+
+}
+
+pub fn semantic_tokens_full(params: &SemanticTokensParams) -> tower_lsp::jsonrpc::Result<Option<SemanticTokensRangeResult>>
+{
+
+    /*
+    match read_to_string( params.url.into())
+    {
+
+        Ok()
+
+    }  
+    */
+
+    //let file_string;
+    
+    match read_to_string( params.text_document.uri.as_str())
+    {
+
+        Ok(res) => {
+
+            let tokens = parse_text(&res);
+
+
+
+        }
+        Err(err) => {
+
+            let mut err_result = tower_lsp::jsonrpc::Error::internal_error();
+
+            let mut error_kind = "";
+
+            match err.kind() {
+                ErrorKind::NotFound => error_kind = "NotFound",
+                ErrorKind::PermissionDenied => error_kind = "PermissionDenied",
+                ErrorKind::ConnectionRefused => error_kind = "ConnectionRefused",
+                ErrorKind::ConnectionReset => error_kind = "ConnectionReset",
+                ErrorKind::ConnectionAborted => error_kind = "ConnectionAborted",
+                ErrorKind::NotConnected => error_kind = "NotConnected",
+                ErrorKind::AddrInUse => error_kind = "AddrInUse",
+                ErrorKind::AddrNotAvailable => error_kind = "AddrNotAvailable",
+                ErrorKind::BrokenPipe => error_kind = "BrokenPipe",
+                ErrorKind::AlreadyExists => error_kind = "AlreadyExists",
+                ErrorKind::WouldBlock => error_kind = "WouldBlock",
+                ErrorKind::InvalidInput => error_kind = "InvalidInput",
+                ErrorKind::InvalidData => error_kind = "InvalidData",
+                ErrorKind::TimedOut => error_kind = "TimedOut",
+                ErrorKind::WriteZero => error_kind = "WriteZero",
+                ErrorKind::Interrupted => error_kind = "Interrupted",
+                ErrorKind::Other => error_kind = "Other",
+                ErrorKind::UnexpectedEof => error_kind = "UnexpectedEof",
+                _ => {}
+            }
+
+            //err_result.data.
+
+            err_result.message = error_kind.to_string();
+
+            return Err(err_result);
+
+        }
+
+    }
+
+
+
+    tower_lsp::jsonrpc::Result::Ok(None)
 
 }
