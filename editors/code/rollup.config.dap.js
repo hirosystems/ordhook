06917f17c0647cfa0a85a1592aee47ffe7807a43
Ludo Galabru <ludovic@Ludos-MacBook-Pro.local>
2022-05-12 15:30:43 -0400

    fix: dap configuration

---
+++ editors/code/rollup.config.dap.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeBuiltins from 'builtin-modules';
+
+/** @type { import('rollup').RollupOptions } */
+export default {
+    input: 'debug/debug.js',
+    plugins: [
+        resolve({
+            preferBuiltins: true
+        }),
+        commonjs({
+            namedExports: {
+                // squelch missing import warnings
+                'vscode-languageclient': ['CreateFile', 'RenameFile', 'ErrorCodes', 'WorkDoneProgress', 'WorkDoneProgressBegin', 'WorkDoneProgressReport', 'WorkDoneProgressEnd']
+            }
+        })
+    ],
+    external: [...nodeBuiltins, 'vscode'],
+    output: {
+        file: './out/debug.js',
+        format: 'cjs',
+        exports: 'named'
+    }
+};
