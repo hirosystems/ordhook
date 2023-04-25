64f726cf710164c9d7ed84eec973ceadef1aa811
Rafael Cárdenas <rafael@rafaelcr.com>
2023-04-25 11:46:10 -0600

    docs: comments

--- components/client/typescript/src/index.ts
+++ components/client/typescript/src/index.ts
@@ -7,6 +7,17 @@ import {
   buildServer,
 } from './server';
 
+/**
+ * Local web server that registers predicates and receives events from a Chainhook node. It handles
+ * retry logic and node availability transparently and provides a callback for individual event
+ * processing.
+ *
+ * Predicates registered here do not accept a `then_that` entry as this will be configured
+ * automatically to redirect events to this server.
+ *
+ * Events relayed by this component will include the original predicate's UUID so actions can be
+ * taken for each relevant predicate.
+ */
 export class ChainhookEventServer {
   private fastify?: FastifyInstance;
   private serverOpts: ServerOptions;
@@ -17,13 +28,21 @@ export class ChainhookEventServer {
     this.chainhookOpts = chainhookOpts;
   }
 
-  async start(predicates: [ServerPredicate], callback: OnEventCallback) {
+  /**
+   * Start the Chainhook event server.
+   * @param predicates - Predicates to register
+   * @param callback - Function to handle every Chainhook event payload sent by the node
+   */
+  async start(predicates: [ServerPredicate], callback: OnEventCallback): Promise<void> {
     if (this.fastify) return;
     this.fastify = await buildServer(this.serverOpts, this.chainhookOpts, predicates, callback);
-    return this.fastify.listen({ host: this.serverOpts.hostname, port: this.serverOpts.port });
+    await this.fastify.listen({ host: this.serverOpts.hostname, port: this.serverOpts.port });
   }
 
-  async close() {
+  /**
+   * Stop the Chainhook event server gracefully.
+   */
+  async close(): Promise<void> {
     await this.fastify?.close();
     this.fastify = undefined;
   }
--- components/client/typescript/src/server.ts
+++ components/client/typescript/src/server.ts
@@ -9,11 +9,12 @@ import { Server } from 'http';
 import { request } from 'undici';
 import { logger, PINO_CONFIG } from './util/logger';
 import { timeout } from './util/helpers';
-import { Payload, PayloadSchema } from './schemas';
+import { Payload, PayloadSchema } from './schemas/payload';
 import { Predicate, PredicateHeaderSchema, ThenThatHttpPost } from './schemas/predicate';
 import { BitcoinIfThisOptionsSchema, BitcoinIfThisSchema } from './schemas/bitcoin/if_this';
 import { StacksIfThisOptionsSchema, StacksIfThisSchema } from './schemas/stacks/if_this';
 
+/** Function type for a Chainhook event callback */
 export type OnEventCallback = (uuid: string, payload: Payload) => Promise<void>;
 
 const ServerOptionsSchema = Type.Object({
@@ -22,12 +23,14 @@ const ServerOptionsSchema = Type.Object({
   auth_token: Type.String(),
   external_host: Type.String(),
 });
+/** Local event server connection and authentication options */
 export type ServerOptions = Static<typeof ServerOptionsSchema>;
 
 const ChainhookNodeOptionsSchema = Type.Object({
   hostname: Type.String(),
   port: Type.Integer(),
 });
+/** Chainhook node connection options */
 export type ChainhookNodeOptions = Static<typeof ChainhookNodeOptionsSchema>;
 
 const IfThisThenNothingSchema = Type.Union([
@@ -57,8 +60,17 @@ const ServerPredicateSchema = Type.Composite([
     ]),
   }),
 ]);
+/** Chainhook predicates registerable by the local event server */
 export type ServerPredicate = Static<typeof ServerPredicateSchema>;
 
+/**
+ * Build the Chainhook Fastify event server.
+ * @param serverOpts - Server options
+ * @param chainhookOpts - Chainhook node options
+ * @param predicates - Predicates to register
+ * @param callback - Event callback function
+ * @returns Fastify instance
+ */
 export async function buildServer(
   serverOpts: ServerOptions,
   chainhookOpts: ChainhookNodeOptions,
@@ -68,7 +80,7 @@ export async function buildServer(
   const base_path = `http://${chainhookOpts.hostname}:${chainhookOpts.port}`;
 
   async function waitForNode(this: FastifyInstance) {
-    logger.info(`EventServer connecting to chainhook node...`);
+    logger.info(`EventServer connecting to chainhook node at ${base_path}...`);
     while (true) {
       try {
         await request(`${base_path}/ping`, { method: 'GET', throwOnError: true });
@@ -81,7 +93,7 @@ export async function buildServer(
   }
 
   async function registerPredicates(this: FastifyInstance) {
-    logger.info(predicates, `EventServer registering predicates on ${base_path}...`);
+    logger.info(predicates, `EventServer registering predicates at ${base_path}...`);
     for (const predicate of predicates) {
       const thenThat: ThenThatHttpPost = {
         http_post: {
@@ -107,7 +119,7 @@ export async function buildServer(
   }
 
   async function removePredicates(this: FastifyInstance) {
-    logger.info(`EventServer closing predicates...`);
+    logger.info(`EventServer closing predicates at ${base_path}...`);
     for (const predicate of predicates) {
       try {
         await request(`${base_path}/v1/chainhooks/${predicate.chain}/${predicate.uuid}`, {
