21846c14f35a315bb1c13a465cfd4bc106cf98b6
Ludo Galabru <ludovic@galabru.com>
2024-01-16 14:12:35 -0500

    feat: migrate to jemallocator

--- components/ordhook-cli/src/main.rs
+++ components/ordhook-cli/src/main.rs
@@ -4,6 +4,13 @@ extern crate serde_derive;
 #[macro_use]
 extern crate hiro_system_kit;
 
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 pub mod cli;
 pub mod config;
 
