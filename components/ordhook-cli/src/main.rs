7d048a3128fc4809950c2aca85c7b139119a05c1
Ludo Galabru <ludovic@galabru.com>
2024-01-18 09:10:48 -0500

    feat: migrate to tcmalloc

--- components/ordhook-cli/src/main.rs
+++ components/ordhook-cli/src/main.rs
@@ -7,6 +7,10 @@ extern crate hiro_system_kit;
 pub mod cli;
 pub mod config;
 
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: tcmalloc2::TcMalloc = tcmalloc2::TcMalloc;
+
 fn main() {
     cli::main();
 }
