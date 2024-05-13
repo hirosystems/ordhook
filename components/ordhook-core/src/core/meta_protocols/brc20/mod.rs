1f23d1b01dce2f5a008c8d15199038c32f4a8ee9
Rafael Cardenas <rafael@rafaelcr.com>
2024-05-13 10:36:13 -0600

    fix: set height 0 for regtest, etc.

--- components/ordhook-core/src/core/meta_protocols/brc20/mod.rs
+++ components/ordhook-core/src/core/meta_protocols/brc20/mod.rs
@@ -9,17 +9,17 @@ pub mod test_utils;
 pub fn brc20_activation_height(network: &BitcoinNetwork) -> u64 {
     match network {
         BitcoinNetwork::Mainnet => 779832,
-        BitcoinNetwork::Regtest => todo!(),
-        BitcoinNetwork::Testnet => todo!(),
-        BitcoinNetwork::Signet => todo!(),
+        BitcoinNetwork::Regtest => 0,
+        BitcoinNetwork::Testnet => 0,
+        BitcoinNetwork::Signet => 0,
     }
 }
 
 pub fn brc20_self_mint_activation_height(network: &BitcoinNetwork) -> u64 {
     match network {
         BitcoinNetwork::Mainnet => 837090,
-        BitcoinNetwork::Regtest => todo!(),
-        BitcoinNetwork::Testnet => todo!(),
-        BitcoinNetwork::Signet => todo!(),
+        BitcoinNetwork::Regtest => 0,
+        BitcoinNetwork::Testnet => 0,
+        BitcoinNetwork::Signet => 0,
     }
 }
